332b5fd13f7299d47727387bb7298cdffb4fbb3b
Skripsi Scamsearcher <Ikhsan Noor Rosyidin>
2015-08-06 00:50:54 +0700

    fix custom command updateestendhomepage, set whitelist value to True only when the estenddomain whitelist value is True

---
+++ website_analyzer/management/commands/updateextendhomepage.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from website_analyzer.models import ExtendHomepage, ExtendDomain
+
+
+class Command(BaseCommand):
+    args = 'Arguments is not needed'
+    help = 'Executing string analyst to homepage'
+
+    def handle(self, *args, **options):
+        ext_doms = ExtendDomain.objects.filter(whitelist=True)
+        for ext_dom in ext_doms:
+            if ext_dom.whitelist:
+                ext_hps = ExtendHomepage.objects.filter(
+                             homepage__domain=ext_dom.domain)
+                for ext_hp in ext_hps:
+                    ext_hp.whitelist = ext_dom.whitelist
+                    ext_hp.save()
