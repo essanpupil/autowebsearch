5fa81b93cd01b851493b488ddccd9a2a12e4ac6d
Skripsi Scamsearcher <Ikhsan Noor Rosyidin>
2015-08-22 15:39:35 +0700

    print "Query database empty" when the query table is empty

--- website_management/management/commands/searchwebpages.py
+++ website_management/management/commands/searchwebpages.py
@@ -10,16 +10,19 @@ class Command(BaseCommand):
     help = "Search webpages with saved queries"
     
     def handle(self, *args, **options):
-        query = Query.objects.all().order_by('times_used')[0]
-        search = GoogleSearch(query.keywords)
-        try:
-            search.start_search()
-            add_list_url_to_webpage(search.search_result)
-            for url in search.search_result:
-                webpage = Webpage.objects.get(url=url)
-                saved_search = Search.objects.create(webpage=webpage,
-                                                     query=query) 
-            query.times_used += 1
-            query.save()
-        except:
-            print "Search failed";
+        query = Query.objects.all().order_by('times_used')
+        if query:
+            search = GoogleSearch(query[0].keywords)
+            try:
+                search.start_search()
+                add_list_url_to_webpage(search.search_result)
+                for url in search.search_result:
+                    webpage = Webpage.objects.get(url=url)
+                    saved_search = Search.objects.create(webpage=webpage,
+                                                         query=query) 
+                query.times_used += 1
+                query.save()
+            except:
+                print "Search failed";
+        else:
+            print "Query database empty"
