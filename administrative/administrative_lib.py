1ed017c83320c3a62a5add2c0fe4b55f570d1e98
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-03-25 22:56:49 +0700

    PEP-ing & apply pylint to code

--- administrative/administrative_lib.py
+++ administrative/administrative_lib.py
@@ -1,22 +1,17 @@
-from website_management.management_lib import add_url_to_webpage 
+"""helper module for administrative app."""
+from website_management.management_lib import add_url_to_webpage
 from website_management.models import Webpage
 
-from .models import Client, Website
+from administrative.models import Client, Website
 
 
 def save_client(name=None, email=None, phone=None, address=None):
-    """
-    Save submited client to database.
-    """
-    Client.objects.create(name=name, email=email, phone=phone,
-                          address=address)
+    """Save submited client to database."""
+    Client.objects.create(name=name, email=email, phone=phone, address=address)
 
 
 def save_client_homepage(client, url, event=None):
-    """
-    Save homepage for this specific client.
-    """
+    """Save homepage for this specific client."""
     add_url_to_webpage(url)
     webpage = Webpage.objects.get(url=url)
-    Website.objects.create(homepage=webpage.homepage, client=client,
-                           event=event)
+    Website.objects.create(homepage=webpage.homepage, client=client, event=event)
