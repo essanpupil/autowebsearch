e62d6ff48c025e541f036986c521db77a37261f0
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-03-27 11:54:40 +0700

    modify travis config to use specific test settings file for testing & move django_nose from common requirements to testing requirements

---
+++ ScamSearcher/test_settings.py
@@ -0,0 +1,24 @@
+"""This is additional settings config for testing only."""
+from ScamSearcher.settings import *
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'website_management',
+    'website_analyzer',
+    'administrative',
+    'django_nose',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=administrative,webscraper,website_analyzer,website_management',
+    '--cover-inclusive',
+]
