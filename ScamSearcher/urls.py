5540c7ed7d3cbdac3355cc0d1289ab9c049840a5
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-03-30 23:12:45 +0700

    modify code to remove line too long warning

--- ScamSearcher/urls.py
+++ ScamSearcher/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-from ScamSearcher.views import login, logout, welcome, user_profile, password_changed
+from ScamSearcher.views import login, logout, welcome, user_profile, \
+                               password_changed
 
 
 urlpatterns = (
