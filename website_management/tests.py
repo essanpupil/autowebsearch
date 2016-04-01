8595377fccdcb5e33b41080252d2aebfedadf659
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-04-01 23:24:21 +0700

    Add unit testing for homepage view in website_maangement app

--- website_management/tests.py
+++ website_management/tests.py
@@ -1,6 +1,6 @@
 "Testing module for website_management app."
-from django.core.urlresolvers import resolve
-from django.test import TestCase
+from django.core.urlresolvers import resolve, reverse
+from django.test import TestCase, Client
 
 from website_management.views import website_dashboard
 
@@ -11,3 +11,16 @@ class HomePageTest(TestCase):
         "resolve homepage url"
         found = resolve('/')
         self.assertEqual(found.func, website_dashboard)
+
+
+class HomePageResponseTest(TestCase):
+    "testing homepage using http request"
+    def setUp(self):
+        "define default test client"
+        self.client = Client()
+
+    def test_request_homepage(self):
+        "test requesting homepage"
+        response = self.client.get(reverse('welcome'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'website_management/dashboard.html')
