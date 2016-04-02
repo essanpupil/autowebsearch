9200b20a61c4d5005f18a90b8bc76534b1fb094b
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-04-02 13:27:39 +0700

    Add functional test to click link to view all websites & passed the test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,6 @@
 "functional tes module"
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
@@ -21,3 +22,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.assertIn('Welcome to ScamSearcher Project', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('SCAM SEARCHER', header_text)
+
+    def test__view_all_websites(self):
+        "click link to view all websites"
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.LINK_TEXT, 'View all websites').click()
+        self.assertIn('View all websites', self.browser.title)
