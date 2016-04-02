0daa5da696c961ca36ba21847a73c0f06a432903
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2016-04-02 22:15:10 +0700

    Applying pylint & flake8

--- website_analyzer/filters.py
+++ website_analyzer/filters.py
@@ -1,17 +1,22 @@
+"filter module for views in website_analyzer app."
 import django_filters
 
 from website_management.models import Domain, Homepage
 
 
 class ExtendDomainFilter(django_filters.FilterSet):
+    "filter based on ExtendDomain model"
     name = django_filters.CharFilter(lookup_type='icontains')
+
     class Meta:
         model = Domain
-        fields = ['name','extenddomain__free', 'extenddomain__whitelist']
+        fields = ['name', 'extenddomain__free', 'extenddomain__whitelist']
 
 
 class ExtendHomepageFilter(django_filters.FilterSet):
+    "filter based on ExtendHomepage model"
     name = django_filters.CharFilter(lookup_type='icontains')
+
     class Meta:
         model = Homepage
         fields = ['name',
